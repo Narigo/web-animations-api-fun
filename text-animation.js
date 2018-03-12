d08a39b49042dda2ee689f87cb5c5b9d801a36aa
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-03-12 02:50:43 +0100

    Add some textual animations
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- text-animation.js
+++ text-animation.js
@@ -1,7 +1,7 @@
 const $text = document.getElementById("text");
 
-const leftText = "Some";
-const rightText = "Text";
+const leftText = "Web";
+const rightText = "Animations";
 
 const leftElements = createElements($text, leftText);
 const rightElements = createElements($text, rightText);
@@ -16,6 +16,78 @@ const rights = {
   width: rightElements.reduce((w, e) => e.width + w, 0)
 };
 
+const ANIMATION_LENGTH = 500;
+
+lefts.elements.forEach((element, idx) => {
+  if (idx > 0) {
+    element.$span.animate([
+        {
+          transformOrigin: "center",
+          transform: "translate3D(0, 0, 0) rotate(90deg)",
+
+        },
+        {
+          transformOrigin: `center ${element.height - (element.height / 3)}px`,
+          transform: `translate3D(${element.left - lefts.width}px, 0, 0) rotate(0deg)`,
+        }
+      ],
+      {
+        duration: ANIMATION_LENGTH,
+        iterations: 1,
+        fill: "forwards"
+      });
+  } else {
+    element.$span.animate([
+      {
+        transformOrigin: "center",
+        transform: "translate3D(0, 0, 0)",
+      },
+      {
+        transformOrigin: "center",
+        transform: `translate3D(${element.left - lefts.width}px, 0, 0)`,
+      }
+    ], {
+      duration: ANIMATION_LENGTH,
+      iterations: 1,
+      fill: "forwards"
+    });
+  }
+});
+
+rights.elements.forEach((element, idx) => {
+  if (0 < idx && idx < rights.elements.length - 1) {
+    element.$span.animate([
+        {
+          transformOrigin: `center`,
+          transform: "translate3D(0, 0, 0) rotate(270deg)",
+        },
+        {
+          transformOrigin: `center ${element.height - (element.height / 3)}px`,
+          transform: `translate3D(${element.left}px, 0, 0) rotate(360deg)`,
+        }
+      ],
+      {
+        duration: ANIMATION_LENGTH,
+        iterations: 1,
+        fill: "forwards"
+      });
+  } else {
+    element.$span.animate([
+      {
+        transformOrigin: "center",
+        transform: "translate3D(0, 0, 0)",
+      },
+      {
+        transformOrigin: "center",
+        transform: `translate3D(${element.left}px, 0, 0)`,
+      }
+    ], {
+      duration: ANIMATION_LENGTH,
+      iterations: 1,
+      fill: "forwards"
+    });
+  }
+});
 
 function createElements($outerWrap, text) {
   const $wrapper = document.createElement("span");
@@ -27,8 +99,10 @@ function splitIntoElements($wrapper, text) {
   const letters = text.split("");
   const $firstSpan = toSpan(letters[0], 0);
   $wrapper.append($firstSpan);
+  $firstSpan.style.position = "absolute";
   const firstElement = {
     left: 0,
+    height: $firstSpan.offsetHeight,
     width: $firstSpan.offsetWidth,
     $span: $firstSpan
   };
@@ -36,12 +110,14 @@ function splitIntoElements($wrapper, text) {
     (acc, letter, idx) => {
       const lastElement = acc[acc.length - 1];
       const $span = toSpan(letter, idx + 1);
+      $span.style.position = "absolute";
       $wrapper.append($span);
       return [
         ...acc,
         {
           left: lastElement.left + lastElement.width,
           width: $span.offsetWidth,
+          height: $span.offsetHeight,
           $span: $span
         }
       ];
